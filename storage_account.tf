aa8aecb4853f38bb6b150d2e7579a9e451c9a7a6
Guilherme Bugallo <67977144+guibugallo@users.noreply.github.com>
2024-05-26 04:44:15 -0300

    Update storage_account.tf

--- storage_account.tf
+++ storage_account.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "resource_group" {
-  name     = "rg-terraform-state"
+  name     = "nome-de-sua-preferência"
   location = var.location
 
   tags = local.common_tags
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "gbugalloterraformstate"
+  name                     = "nomedesuapreferência"
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = var.account_tier
@@ -19,8 +19,8 @@ resource "azurerm_storage_account" "storage_account" {
   tags = local.common_tags
 }
 
-resource "azurerm_storage_container" "container" {
-  name                  = "remote-state-gbugallo"
+resource "azurerm_storage_container" "nome-de-sua-preferência-para-o-resource-container" {
+  name                  = "nome-de-sua-preferência-para-o-container"
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
