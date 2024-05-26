07a1367f895cbdbadcec0775a0e2224e23313cbc
Guilherme Bugallo <67977144+guibugallo@users.noreply.github.com>
2024-05-26 04:41:36 -0300

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "versionamento-bucket" {
-  bucket = "remote-state-gbugallo-bucket"
+resource "aws_s3_bucket" "nome-de-sua-preferência-para-o-versionamento" {
+  bucket = "nome-de-sua-preferência-para-o-bucket"
 }
 
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.versionamento-bucket.id
+resource "aws_s3_bucket_versioning" "nome-de-sua-preferência" {
+  bucket = aws_s3_bucket.nome-de-sua-preferência-para-o-versionamento.id
   versioning_configuration {
     status = "Enabled"
   }
